ecc119b44a7b53020016fa3f25cb77e7de6200d4
ristosip <59021925+ristosip@users.noreply.github.com>
2021-08-14 15:54:01 +0300

    Update RJS Sampling - Increase Leading Pad Length.lua

--- RJS Sampling - Increase Leading Pad Length.lua
+++ RJS Sampling - Increase Leading Pad Length.lua
@@ -1,4 +1,4 @@
--- This script is a part of 'Sampling Suite' designed to automate tasks related to sample instrument creation.
+-- This script is a part of 'RJS Sampling Suite' designed to automate tasks related to sample instrument creation.
 -- Running the script adjusts the starting point of the sample.
 -- 'Chop Samples' script should be run before running this script as it links the samples and the cut out pieces that this script adjusts.
 -- The script always adjusts the sample, meaning 'leading pad' refers to the 'leading pad' of the sample, not the cut out piece!
