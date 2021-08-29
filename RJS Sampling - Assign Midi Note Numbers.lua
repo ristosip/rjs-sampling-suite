a213cd535f361f79307c12db22d63fd3a043c402
ristosip <59021925+ristosip@users.noreply.github.com>
2021-08-29 18:40:24 +0300

    Update RJS Sampling - Assign Midi Note Numbers.lua

--- RJS Sampling - Assign Midi Note Numbers.lua
+++ RJS Sampling - Assign Midi Note Numbers.lua
@@ -1,3 +1,6 @@
+-- Copyright (C) 2021 Risto Sipola
+-- License: See LICENSE.txt
+--
 -- This script is a part of 'RJS Sampling Suite' designed to automate tasks related to sample instrument creation.
 -- Running the script labels the samples based on timeline markers that indicate midi note numbers.
 --
