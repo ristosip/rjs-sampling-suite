ab4ef760b3598275b8504f0685ce1998029228f1
ristosip <59021925+ristosip@users.noreply.github.com>
2021-08-29 18:46:05 +0300

    Update RJS Sampling - Update Sample Info.lua

--- RJS Sampling - Update Sample Info.lua
+++ RJS Sampling - Update Sample Info.lua
@@ -1,3 +1,6 @@
+-- Copyright (C) 2021 Risto Sipola
+-- License: See LICENSE.txt
+--
 -- This script is a part of 'RJS Sampling Suite' designed to automate tasks related to sample instrument creation.
 -- Running the script updates the midi note labelling of the samples.
 -- author: Risto Sipola
