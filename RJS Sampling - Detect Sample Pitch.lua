ae5956e2871f4656203d4355284de22aa04d3ac1
ristosip <59021925+ristosip@users.noreply.github.com>
2021-08-30 15:41:34 +0300

    Update RJS Sampling - Detect Sample Pitch.lua

--- RJS Sampling - Detect Sample Pitch.lua
+++ RJS Sampling - Detect Sample Pitch.lua
@@ -1,8 +1,8 @@
--- Copyright (C) 2021 Risto Sipola
--- License: See LICENSE.txt
---
 -- This script is a part of 'RJS Sampling Suite' designed to automate tasks related to sample instrument creation.
 -- Running the script detects the pitches (closest musical note) of the samples and labels the samples accordingly. In addition, the script creates a tuning aid track (See details below).
+--
+-- Copyright (C) 2021 Risto Sipola
+-- 'RJS Sampling Suite' script collection is licensed under the GNU General Public License v3.0: See LICENSE.txt
 --
 -- How to use: 
 --             1. Place the samples on the first track of the project. Ideally you have run the 'Chop Samples' script and are ready to go.
